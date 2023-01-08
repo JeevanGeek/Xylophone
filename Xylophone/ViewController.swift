56a588f1780bbf514ca120a281af6c7d8b3da520
Jeevan Joshi <jeevanc@geekyants.com>
2023-01-08 10:44:03 +0530

    add xylophone instrument

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,15 +5,23 @@
 //  Created by Jeevan Chandra Joshi on 08/01/23.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let url = Bundle.main.url(forResource: sender.currentTitle, withExtension: "wav") else {
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+        } catch {}
+        player?.play()
+    }
 }
-
